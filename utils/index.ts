8b0f496a1ce1c2589970ab3635d3d9b849cd7f50
daniel petcov <danielpetcov.work@gmail.com>
2025-09-08 00:03:06 +0300

    added env :)

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,12 @@
 export async function fetchCars() {
+  const RAPID_KEY = process.env.X_RAPIDAPI_KEY;
+  const RAPID_HOST = process.env.X_RAPIDAPI_HOST;
+
+  if (!RAPID_KEY || !RAPID_HOST) return null;
+
   const headers = {
-    "x-rapidapi-key": "f3c5121b21msh9a789eab36a277ap1a58a2jsnb3a5a1448ae5",
-    "x-rapidapi-host": "cars-by-api-ninjas.p.rapidapi.com",
+    "x-rapidapi-key": RAPID_KEY,
+    "x-rapidapi-host": RAPID_HOST,
   };
 
   const url = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla`;
